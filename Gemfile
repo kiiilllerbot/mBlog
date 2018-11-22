721a27dcee635eeb5cf8ec440cb53ac5a3ab2ce8
Shimol-Khan <se.shimolkhan@gmail.com>
2018-11-23 00:46:32 +0800

    Added And Configured pg Gem

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails', '~> 5.1.6'
-gem 'sqlite3'
+#gem 'sqlite3'
+gem 'pg', '~> 1.1', '>= 1.1.3'
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -12,25 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20181122153755) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "adminpack"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "body"
-    t.integer "post_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -42,8 +34,6 @@ ActiveRecord::Schema.define(version: 20181122153755) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
-    t.string "slug"
-    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +48,5 @@ ActiveRecord::Schema.define(version: 20181122153755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
