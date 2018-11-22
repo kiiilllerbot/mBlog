e7a68a70e96818bc1f679e0b0d17afaa647b2aec
Shimol-Khan <se.shimolkhan@gmail.com>
2018-11-22 22:53:04 +0800

    Fixed Active State of Navbar Links

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	resources :posts do
 		resources :comments
 	end
+	get 'pages/about' => 'pages/about', as: :about
+	get 'pages/contact' => 'pages/contact', as: :contact
 end
